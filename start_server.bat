4c9bf22814255238f781e395032b83f18589801f
Rajkumardadri <122042865+Rajkumardadri@users.noreply.github.com>
2026-08-30 14:30:15 +0530

    Add files via upload

--- start_server.bat
+++ start_server.bat
@@ -1,10 +1,5 @@
 @echo off
-echo =========================================================
-echo UP VAAD Court Order Clean Print Server & Web Application
-echo =========================================================
-echo Starting Python backend server on http://localhost:8080 ...
-echo.
-
-"C:\Users\ss\AppData\Roaming\uv\python\cpython-3.13.5-windows-x86_64-none\python.exe" server.py
-
+title UP VAAD Clean Order Server (Port 9091)
+echo Starting UP VAAD Clean Order Local Proxy Server on Port 9091...
+python server.py
 pause
